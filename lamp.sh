a883a449ba6629a24f28852230b19af97d35ed0a
Mike <bat9r.dzuno@gmail.com>
2017-09-12 13:48:53 +0300

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -5,7 +5,7 @@ then
 	echo "Y" | sudo apt-get install apache2
 	sudo systemctl start apache2
 	echo "Y" | sudo apt-get install mysql-server mysql-client
-	echo "Y" sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
+	echo "Y" | sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
 else
 	echo "Try sudo $(basename $0)"
 fi
