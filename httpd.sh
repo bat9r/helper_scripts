dce0c5d0b6850671940e1840242a5cc41867784a
Mike <bat9r@localhost.localdomain>
2017-09-14 16:44:28 +0300

    Add Fedora compatibility

--- httpd.sh
+++ httpd.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
-if ! pidof apache2 > /dev/null
+if [ -f /etc/debian_version ]
 then
-   sudo service apache2 restart
-else
-   echo "All is well"
+	if ! pidof apache2 > /dev/null
+	then
+	   sudo service apache2 restart
+	else
+	   echo "All is well"
+	fi
+elif [ -f /etc/redhat-release ]
+then
+	if ! pidof httpd > /dev/null
+	then
+	   sudo service httpd restart
+	else
+	   echo "All is well"
+	fi
 fi
+	
+
