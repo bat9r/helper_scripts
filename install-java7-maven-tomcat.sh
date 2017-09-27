b5feb3c0e79cbe3680ab1471141cc512dda274b5
Mike <bat9r.dzuno@gmail.com>
2017-09-27 21:18:16 +0300

    Update install-java7-maven-tomcat.sh

--- install-java7-maven-tomcat.sh
+++ install-java7-maven-tomcat.sh
@@ -8,7 +8,7 @@ sudo alternatives --config java
 
 #Install maven
 cd /opt
-sudo yum install wget
+echo "y" | sudo yum install wget
 wget http://www-eu.apache.org/dist/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
 sudo tar xzf apache-maven-3.5.0-bin.tar.gz
 sudo ln -s apache-maven-3.5.0  maven
@@ -21,7 +21,7 @@ echo "y" | sudo yum install tomcat
 sudo echo -e 'JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xmx512m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC"' | sudo tee --append /usr/share/tomcat/conf/tomcat.conf
 echo "y" | sudo yum install tomcat-webapps tomcat-admin-webapps 
 echo "y" | sudo yum install tomcat-docs-webapp tomcat-javadoc
-sudo sed '/<\/tomcat-users>/i \n<role rolename="manager-gui"\/>\n<role rolename="manager-script"\/>\n<user username="admin" password="password" roles="manager-gui,admin-gui"\/>\n' /usr/share/tomcat/conf/tomcat-users.xml | sudo tee /usr/share/tomcat/conf/tomcat-users2.xml
+sudo sed '/<\/tomcat-users>/i <role rolename="manager-gui"\/>\n<role rolename="manager-script"\/>\n<user username="admin" password="password" roles="manager-gui,admin-gui"\/>\n' /usr/share/tomcat/conf/tomcat-users.xml | sudo tee /usr/share/tomcat/conf/tomcat-users2.xml
 sudo mv /usr/share/tomcat/conf/tomcat-users2.xml /usr/share/tomcat/conf/tomcat-users.xml
 sudo systemctl start tomcat
 sudo systemctl enable tomcat
