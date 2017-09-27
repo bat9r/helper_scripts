8d7a656faae3a9ff29bd67db2e2e3cf071cd1426
Mike <bat9r.dzuno@gmail.com>
2017-09-27 21:33:00 +0300

    edit user name and pass

--- install-java7-maven-tomcat.sh
+++ install-java7-maven-tomcat.sh
@@ -2,6 +2,10 @@
 
 #CentOS 7 (sudo)
 
+#Tomcat user and pass
+tomcat_user="admin"
+tomcat_password="admin"
+
 #Install java 7
 echo "y" | sudo yum install java-1.7.0-openjdk
 sudo alternatives --config java
@@ -21,11 +25,11 @@ echo "y" | sudo yum install tomcat
 sudo echo -e 'JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xmx512m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC"' | sudo tee --append /usr/share/tomcat/conf/tomcat.conf
 echo "y" | sudo yum install tomcat-webapps tomcat-admin-webapps 
 echo "y" | sudo yum install tomcat-docs-webapp tomcat-javadoc
-sudo sed '/<\/tomcat-users>/i <role rolename="manager-gui"\/>\n<role rolename="manager-script"\/>\n<user username="admin" password="password" roles="manager-gui,admin-gui"\/>\n' /usr/share/tomcat/conf/tomcat-users.xml | sudo tee /usr/share/tomcat/conf/tomcat-users2.xml
+sudo sed '/<\/tomcat-users>/i <role rolename="manager-gui"\/>\n<role rolename="manager-script"\/>\n<user username="$tomcat_user" password="$tomcat_password" roles="manager-gui,admin-gui"\/>\n' /usr/share/tomcat/conf/tomcat-users.xml | sudo tee /usr/share/tomcat/conf/tomcat-users2.xml
 sudo mv /usr/share/tomcat/conf/tomcat-users2.xml /usr/share/tomcat/conf/tomcat-users.xml
 sudo systemctl start tomcat
 sudo systemctl enable tomcat
 
 #Configure tomcat+maven (deploy)
-sudo sed '/<\/servers>/i <server>\n<id>TomcatServer<\/id>\n<username>admin<\/username>\n<password>password<\/password>\n<\/server>\n' /opt/maven/conf/settings.xml | /opt/maven/conf/settings2.xml
+sudo sed '/<\/servers>/i <server>\n<id>TomcatServer<\/id>\n<username>$tomcat_user<\/username>\n<password>$tomcat_password<\/password>\n<\/server>\n' /opt/maven/conf/settings.xml | /opt/maven/conf/settings2.xml
 sudo mv /opt/maven/conf/settings2.xml /opt/maven/conf/settings.xml
