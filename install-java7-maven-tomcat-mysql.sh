4839c72ae6755e39c95b7eeef272acd4f448f197
Mike <bat9r.dzuno@gmail.com>
2017-09-28 17:18:32 +0300

    Update install-java7-maven-tomcat-mysql.sh

--- install-java7-maven-tomcat-mysql.sh
+++ install-java7-maven-tomcat-mysql.sh
@@ -23,6 +23,7 @@ sudo tar xzf /opt/apache-maven-3.5.0-bin.tar.gz
 sudo ln -s /opt/apache-maven-3.5.0  maven
 sudo touch /etc/profile.d/maven.sh
 sudo echo -e '\n export M2_HOME=/opt/maven \n export PATH=${M2_HOME}/bin:${PATH} ' | sudo tee --append /etc/profile.d/maven.sh
+source /etc/profile.d/maven.sh
 rm -f /opt/apache-maven-3.5.0-bin.tar.gz
 
 #Install tomcat
