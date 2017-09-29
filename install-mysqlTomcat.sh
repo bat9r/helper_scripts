ca3d2d6f756fb06bb6259534ae65d993fe4b9db1
Mike <bat9r.dzuno@gmail.com>
2017-09-29 15:19:51 +0300

    Create install-mysqlTomcat.sh

---
+++ install-mysqlTomcat.sh
@@ -0,0 +1,15 @@
+#Install MySQL
+
+mysql_user="tom"
+mysql_password="cat"
+first_db="test_oms_db"
+second_db="product_oms_db"
+
+echo "y" | sudo yum install mariadb mariadb-server
+sudo systemctl enable mariadb
+sudo systemctl start mariadb
+mysql -u root -e "CREATE USER '$mysql_user'@'%' IDENTIFIED BY '$mysql_password';
+                  GRANT ALL PRIVILEGES ON * . * TO '$mysql_user'@'%';
+                  FLUSH PRIVILEGES;"
+mysql -u $mysql_user -p${mysql_password} -e "create database $first_db;
+                                             create database $second_db;"
