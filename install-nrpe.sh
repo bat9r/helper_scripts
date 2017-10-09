d5b41bcd321fe8b0c95d1f4f572ab1ef76ab1000
Mike <bat9r.dzuno@gmail.com>
2017-10-09 17:44:05 +0300

    Update install-nrpe.sh

--- install-nrpe.sh
+++ install-nrpe.sh
@@ -1,25 +1,25 @@
-mkdir ~/downloads
+sudo mkdir ~/downloads
 cd ~/downloads
-wget http://www.nagios-plugins.org/download/nagios-plugins-2.1.1.tar.gz
-tar xzf nagios-plugins-2.1.1.tar.gz
+sudo wget http://www.nagios-plugins.org/download/nagios-plugins-2.1.1.tar.gz
+sudo tar xzf nagios-plugins-2.1.1.tar.gz
 cd nagios-plugins-2.1.1
 ./configure
-make
-make install
-chown nagios.nagios /usr/local/nagios
-chown -R nagios.nagios /usr/local/nagios/libexec
+sudo make
+sudo make install
+sudo chown nagios.nagios /usr/local/nagios
+sudo chown -R nagios.nagios /usr/local/nagios/libexec
 sudo yum install xinetd
 cd ~/downloads
-wget https://github.com/NagiosEnterprises/nrpe/archive/nrpe-3.0.tar.gz
-tar xzf nrpe-3.0.tar.gz
+sudo wget https://github.com/NagiosEnterprises/nrpe/archive/nrpe-3.0.tar.gz
+sudo tar xzf nrpe-3.0.tar.gz
 cd nrpe-nrpe-3.0
-make install-groups-users
-make install
-make install-config
-make install-inetd
-make install-init
-service xinetd restart
-systemctl reload xinetd
-systemctl enable nrpe && systemctl start nrpe
+sudo make install-groups-users
+sudo make install
+sudo make install-config
+sudo make install-inetd
+sudo make install-init
+sudo service xinetd restart
+sudo systemctl reload xinetd
+sudo systemctl enable nrpe && systemctl start nrpe
 sudo firewall-cmd --zone=public --add-port=5666/tcp
 sudo firewall-cmd --zone=public --add-port=5666/tcp --permanent
