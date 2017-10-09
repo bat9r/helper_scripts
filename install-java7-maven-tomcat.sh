843eedb764141717a86c89cfaa674d9fb3720a1d
Mike <bat9r.dzuno@gmail.com>
2017-10-09 20:57:23 +0300

    Update install-java7-maven-tomcat.sh

--- install-java7-maven-tomcat.sh
+++ install-java7-maven-tomcat.sh
@@ -6,7 +6,6 @@
 #Tomcat user and pass
 tomcat_user="admin"
 tomcat_password="admin"
-IP="192.169.103.220"
 
 #Install java 7
 echo "y" | sudo yum install java-1.7.0-openjdk-devel
@@ -29,7 +28,7 @@ rm -f /opt/apache-maven-3.5.0-bin.tar.gz
 #Install tomcat
 echo "y" | sudo yum install httpd
 echo "y" | sudo yum install tomcat
-sudo echo -e 'JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xmx512m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9000 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.rmi.port=9000 -Djava.rmi.server.hostname=$IP"' | sudo tee --append /usr/share/tomcat/conf/tomcat.conf
+sudo echo -e 'JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xmx512m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9000 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.rmi.port=9000"' | sudo tee --append /usr/share/tomcat/conf/tomcat.conf
 echo "y" | sudo yum install tomcat-webapps tomcat-admin-webapps 
 echo "y" | sudo yum install tomcat-docs-webapp tomcat-javadoc
 sudo sed "/<\/tomcat-users>/i <role rolename=\"manager-gui\"\/>\n<role rolename=\"manager-jmx\"\/>\n<role rolename=\"manager\"\/>\n<role rolename=\"manager-script\"\/>\n<user username=\"$tomcat_user\" password=\"$tomcat_password\" roles=\"manager-gui,manager,manager-script,manager-jmx\"\/>\n" /usr/share/tomcat/conf/tomcat-users.xml | sudo tee /usr/share/tomcat/conf/tomcat-users2.xml
