19a4ed6e25c46864fbfbc3e10098558636464638
Mike <bat9r.dzuno@gmail.com>
2017-10-09 15:14:34 +0300

    Open jmx port

--- install-java7-maven-tomcat.sh
+++ install-java7-maven-tomcat.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 #CentOS 7 (sudo)
+#Opens jmx port
 
 #Tomcat user and pass
 tomcat_user="admin"
 tomcat_password="admin"
+IP="192.169.103.220"
 
 #Install java 7
 echo "y" | sudo yum install java-1.7.0-openjdk-devel
@@ -27,13 +29,15 @@ rm -f /opt/apache-maven-3.5.0-bin.tar.gz
 #Install tomcat
 echo "y" | sudo yum install httpd
 echo "y" | sudo yum install tomcat
-sudo echo -e 'JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xmx512m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC"' | sudo tee --append /usr/share/tomcat/conf/tomcat.conf
+sudo echo -e 'JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Djava.awt.headless=true -Xmx512m -XX:MaxPermSize=256m -XX:+UseConcMarkSweepGC -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=9000 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.rmi.port=9000 -Djava.rmi.server.hostname=$IP"' | sudo tee --append /usr/share/tomcat/conf/tomcat.conf
 echo "y" | sudo yum install tomcat-webapps tomcat-admin-webapps 
 echo "y" | sudo yum install tomcat-docs-webapp tomcat-javadoc
 sudo sed "/<\/tomcat-users>/i <role rolename=\"manager-gui\"\/>\n<role rolename=\"manager-jmx\"\/>\n<role rolename=\"manager\"\/>\n<role rolename=\"manager-script\"\/>\n<user username=\"$tomcat_user\" password=\"$tomcat_password\" roles=\"manager-gui,manager,manager-script,manager-jmx\"\/>\n" /usr/share/tomcat/conf/tomcat-users.xml | sudo tee /usr/share/tomcat/conf/tomcat-users2.xml
 sudo mv /usr/share/tomcat/conf/tomcat-users2.xml /usr/share/tomcat/conf/tomcat-users.xml
 sudo firewall-cmd --zone=public --add-port=8080/tcp
 sudo firewall-cmd --zone=public --add-port=8080/tcp --permanent
+sudo firewall-cmd --zone=public --add-port=9000/tcp
+sudo firewall-cmd --zone=public --add-port=9000/tcp --permanent
 sudo systemctl start tomcat
 sudo systemctl enable tomcat
 sudo systemctl start httpd
@@ -47,8 +51,4 @@ sudo setenforce 0
 sudo sed "/<\/servers>/i <server>\n<id>TomcatServer<\/id>\n<username>$tomcat_user<\/username>\n<password>$tomcat_password<\/password>\n<\/server>\n" /opt/maven/conf/settings.xml | sudo tee /opt/maven/conf/settings2.xml
 sudo mv /opt/maven/conf/settings2.xml /opt/maven/conf/settings.xml
 
-#For mysql
-sudo firewall-cmd --zone=public --add-port=3306/tcp
-sudo firewall-cmd --zone=public --add-port=3306/tcp --permanent
-                                             
 
