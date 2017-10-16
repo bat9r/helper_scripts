102430fafe34705db7a82783da7a53e65315b23c
Mike <bat9r.dzuno@gmail.com>
2017-10-16 16:59:23 +0300

    Update install-nagios-plugins.sh

--- install-nagios-plugins.sh
+++ install-nagios-plugins.sh
@@ -18,8 +18,7 @@ cd /tmp/nagios-plugins-release-2.2.1/
 sudo make
 sudo make install
 
-cd /etc/local/nagios/libexec/
-sudo wget https://raw.githubusercontent.com/fridim/nagios-plugin-check_galera_cluster/master/check_galera_cluster
+sudo wget -O /etc/local/nagios/libexec/check_galera_cluster https://raw.githubusercontent.com/fridim/nagios-plugin-check_galera_cluster/master/check_galera_cluster
 chmod +x check_galera_cluster
 #Service / Daemon Command
 sudo systemctl restart nagios.service
