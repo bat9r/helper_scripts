b50437edd051aea2a8294e19a6091cc52bbc52dc
Mike <bat9r.dzuno@gmail.com>
2017-11-02 15:00:28 +0200

    Create open-cloud.sh

---
+++ bridge-xen/open-cloud.sh
@@ -0,0 +1,44 @@
+firewall-cmd --zone=public --add-port=111/tcp
+firewall-cmd --zone=public --add-port=111/tcp --permanent
+
+firewall-cmd --zone=public --add-port=111/udp
+firewall-cmd --zone=public --add-port=111/udp --permanent
+
+firewall-cmd --zone=public --add-port=2049/tcp
+firewall-cmd --zone=public --add-port=2049/tcp --permanent
+
+firewall-cmd --zone=public --add-port=32803/tcp
+firewall-cmd --zone=public --add-port=32803/tcp --permanent
+
+firewall-cmd --zone=public --add-port=32769/udp
+firewall-cmd --zone=public --add-port=32769/udp --permanent
+
+firewall-cmd --zone=public --add-port=892/tcp
+firewall-cmd --zone=public --add-port=892/tcp --permanent
+
+firewall-cmd --zone=public --add-port=892/udp
+firewall-cmd --zone=public --add-port=892/udp --permanent
+
+firewall-cmd --zone=public --add-port=875/tcp
+firewall-cmd --zone=public --add-port=875/tcp --permanent
+
+firewall-cmd --zone=public --add-port=875/udp
+firewall-cmd --zone=public --add-port=875/udp --permanent
+
+firewall-cmd --zone=public --add-port=662/tcp
+firewall-cmd --zone=public --add-port=662/tcp --permanent
+
+firewall-cmd --zone=public --add-port=662/udp
+firewall-cmd --zone=public --add-port=662/udp --permanent
+
+firewall-cmd --zone=public --add-port=8080/tcp
+firewall-cmd --zone=public --add-port=8080/tcp --permanent
+
+firewall-cmd --zone=public --add-port=80/tcp
+firewall-cmd --zone=public --add-port=80/tcp --permanent
+
+firewall-cmd --zone=public --add-port=9000/tcp
+firewall-cmd --zone=public --add-port=9000/tcp --permanent
+
+firewall-cmd --zone=public --add-port=22/tcp
+firewall-cmd --zone=public --add-port=22/tcp --permanent
