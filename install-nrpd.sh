fc66eaeff7ca05a2ca8aa550f3ba6ae3b6aa91b0
Mike <bat9r.dzuno@gmail.com>
2017-11-09 17:01:22 +0200

    Update install-nrpd.sh

--- install-nrpd.sh
+++ install-nrpd.sh
@@ -1,3 +1,7 @@
+#CentOS7
+
+mytoken="1qazxsw23edc"
+
 sudo yum -y install wget
 sudo yum -y install httpd
 sudo yum install php.i686
@@ -8,8 +12,7 @@ sudo cd /tmp/nrdp-1.4.0/
 sudo mkdir -p /usr/local/nrdp
 sudo cp -r clients server LICENSE* CHANGES* /usr/local/nrdp
 sudo chown -R nagios:nagios /usr/local/nrdp 
-#TODO
-#vi /usr/local/nrdp/server/config.inc.php token
+sudo sed -i "s/cfg\['authorized_tokens'\]\ =\ array(/cfg['authorized_tokens'] = array(\"$mytoken\",\ /g" /usr/local/nrdp/server/config.inc.php
 sudo cp nrdp.conf /etc/httpd/conf.d/
 sed -i 's/<\/Directory>/Require all granted\n&/' /etc/httpd/conf.d/nrdp.conf
 systemctl restart httpd.service
